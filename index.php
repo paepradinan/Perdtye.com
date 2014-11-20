5cd0f1ea207254bdaf663235ba635de74675c61b
Pae <pradinaan@gmail.com>
2014-11-20 18:05:18 +0700

    Pae

--- index.php
+++ index.php
@@ -212,7 +212,6 @@ body {
 		</div>
 	</div>
 <!--
-     <p>&nbsp;</p>
 	<div class="row clearfix">
 		<div class="col-md-12 column">
             
@@ -291,39 +290,25 @@ body {
                                                     <div class="col-md-1 column">
                                                     </div>
                                             <div class="col-md-10 column">
-                                                <center>
-									           <img alt="140x140" width="150px" src="i6.png">
-                                                </center>
-     <p>&nbsp;</p>
-									           <dl>
-										<dt>
-											Description lists
-										</dt>
-										<dd>
-											A description list is perfect for defining terms.
-										</dd>
-										<dt>
-											Euismod
-										</dt>
-										<dd>
-											Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.
-										</dd>
-										<dd>
-											Donec id elit non mi porta gravida at eget metus.
-										</dd>
-										<dt>
-											Malesuada porta
-										</dt>
-										<dd>
-											Etiam porta sem malesuada magna mollis euismod.
-										</dd>
-										<dt>
-											Felis euismod semper eget lacinia
-										</dt>
-										<dd>
-											Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-										</dd>
-									</dl>
+                                                <div class="row">
+ 
+    <div class="thumbnail">
+      <img src="i6.png" >
+      <div class="caption">
+        <h3>iPhone6 64gb Gold</h3>
+        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+          <div class="row clearfix">
+              <div class="col-md-6 column">
+                  <a href="#" class="btn btn-primary" role="button">Details</a> 
+              </div>
+              <div class="col-md-6 column navbar-right">
+                  <a href="#" class="btn btn-default" role="button">Buy Now</a>
+              </div>
+          </div>
+<!--        <p style="margin-right:0px"><a href="#" class="btn btn-primary" role="button">Details</a> <a href="#" class="btn btn-default" role="button">Buy Now</a></p>-->
+      </div>
+    </div>
+  </div>
 								            </div>
 								            <div class="col-md-1 column">
 								            </div>
