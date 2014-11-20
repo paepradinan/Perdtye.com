16634f37e735b28889bf36998a09272d6e95dc66
purikuo <PurikuOsk@Piraths-MacBook-Air.local>
2014-11-20 15:44:08 +0700

    Pure
    BackUpIndex

--- index copy.php
+++ index copy.php
@@ -100,7 +100,7 @@
 							<a href="/perdtye/login2.html">Login</a>
 						</li>
                         <li>
-							<a href="/perdtye/signup.html">Signup</a>
+							<a href="#">Signup</a>
 						</li>
 						<li>
 							<a href="#">Sell</a>
@@ -347,7 +347,106 @@
 	</div>
     
             <p>&nbsp;</p>
-	
+	<div class="row clearfix">
+		<div class="col-md-12 column">
+			<nav class="navbar navbar-default" role="navigation">
+                
+                    <ul class="nav navbar-nav">
+						<li>
+							<a href="#">FAQ</a>
+						</li>
+						<li>
+							<a href="#">About</a>
+						</li>
+                        <li>
+							<a href="#">Report</a>
+						</li>
+                        <li>
+							<a href="#">Privacy</a>
+						</li>
+                        <li>
+							<a href="#">Term of use</a>
+						</li>
+                        
+					</ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a id="toTop" href="#top">Back to top</a>
+                        </li>
+                        <li>
+                            <a></a>
+                        </li>
+                    </ul>
+<!--
+				<div class="navbar-header">
+					 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button> <a class="navbar-brand" href="#">Brand</a>
+				</div>
+				
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li class="active">
+							<a href="#">Link</a>
+						</li>
+						<li>
+							<a href="#">Link</a>
+						</li>
+						<li class="dropdown">
+							 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
+							<ul class="dropdown-menu">
+								<li>
+									<a href="#">Action</a>
+								</li>
+								<li>
+									<a href="#">Another action</a>
+								</li>
+								<li>
+									<a href="#">Something else here</a>
+								</li>
+								<li class="divider">
+								</li>
+								<li>
+									<a href="#">Separated link</a>
+								</li>
+								<li class="divider">
+								</li>
+								<li>
+									<a href="#">One more separated link</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+					
+					<ul class="nav navbar-nav navbar-right">
+						<li>
+							<a href="#">Link</a>
+						</li>
+						<li class="dropdown">
+							 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
+							<ul class="dropdown-menu">
+								<li>
+									<a href="#">Action</a>
+								</li>
+								<li>
+									<a href="#">Another action</a>
+								</li>
+								<li>
+									<a href="#">Something else here</a>
+								</li>
+								<li class="divider">
+								</li>
+								<li>
+									<a href="#">Separated link</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+-->
+				
+			</nav>
+		</div>
+	</div>
+</div>
 
 
   <!--BOTTOM-->
